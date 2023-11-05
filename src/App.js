d723652e7823e26d7fc101e629acda1c8ce2b153
vuhongxuong <74437603+vuhongxuong@users.noreply.github.com>
2023-11-05 15:05:18 +0700

    config customize-cra babel plugin module resolver

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
+import Button from './components/Button';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Button />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ src/components/Button/index.js
@@ -0,0 +1,9 @@
+import MyAlert from "~/Alert"
+
+function Button() {
+    return <button onClick={MyAlert}>Click me</button>
+
+}
+
+
+export default Button
