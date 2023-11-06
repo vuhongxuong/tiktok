cc09e4acdc5c1114cd50d1f18105a9595db5b483
vuhongxuong <74437603+vuhongxuong@users.noreply.github.com>
2023-11-06 20:06:26 +0700

    set layout

---
+++ src/routes/index.js
@@ -0,0 +1,21 @@
+//Layout
+import { HeaderOnly } from '~/components/Layout';
+//Pages
+import Home from '~/Pages/Home';
+import Following from '~/Pages/Following';
+import Profile from '~/Pages/Profile';
+import Upload from '~/Pages/Upload';
+import Search from '~/Pages/Search';
+//public routes
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/profile', component: Profile },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: '/search', component: Search, layout: null },
+];
+
+//routes phai dang nhap moi xem duoc
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
